4c425ba3b254eb47c839dcc0f9f4d14a34ffcef9
HtwoO <1549914423@qq.com>
2023-03-14 09:32:27 +0800

    feat(重构): 🎉 重构

--- vite.config.ts
+++ vite.config.ts
@@ -2,13 +2,13 @@
  * @Author: chen qi
  * @Date: 2023-03-13 22:39:10
  * @LastEditors: chen qi
- * @LastEditTime: 2023-03-14 01:59:06
+ * @LastEditTime: 2023-03-14 09:28:41
  * @Description: ~
  */
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { resolve } from "path"
-import pkg from './package.json'
+import pkg from './package.json' 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
